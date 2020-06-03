117188027dbcaf77f1c34d31ea469f445c415562
Tarlochan5268 <brotherstarlochan5268@gmail.com>
2020-06-03 07:34:18 -0400

    CurrencyTest class all the test cases is done

--- test/bankingApp/CurrencyTest.java
+++ test/bankingApp/CurrencyTest.java
@@ -29,31 +29,48 @@ public class CurrencyTest {
 	public void testGetName() {
 		// Write the test case for testing the getName() function
 		assertEquals("CAD",CAD.getName());
-		fail("Write test case here");
+		assertEquals("EUR",EUR.getName());
+		assertEquals("GBP",GBP.getName());
+		assertEquals("HKD",HKD.getName());
+		//fail("Write test case here");
 	}
 	
 	@Test
 	public void testGetRate() {
-		// @TODO: Write the test case for testing the getRate() function
-		fail("Write test case here");
+		assertEquals(0,Double.compare(0.75,CAD.getRate()));
+		assertEquals(1.14,EUR.getRate(),2); // Alternative using Delta
+		assertEquals(1.26,GBP.getRate(),2);
+		assertEquals(0.13,HKD.getRate(),2);
+		//fail("Write test case here");
 	}
 	
 	@Test
 	public void testSetRate() {
 		// @TODO: Write the test case for testing the setRate() function
-		
 		// For this function, you should do:
 		// 1. Assert that the oldRate is correct
 		// 2. Change the rate
 		// 3. Assert that the newRate is correct
 		// You will end up with 2 assert() statements in this function.
-		fail("Write test case here");
+		//fail("Write test case here");
+		assertEquals(0,Double.compare(0.75,CAD.getRate()));
+		CAD.setRate(0.76);
+		assertEquals(0,Double.compare(0.76,CAD.getRate()));
+
 	}
 	
 	@Test
 	public void testValueInUSD() {
 		// @TODO: Write the test case for testing the valueInUSD() function
-		fail("Write test case here");
+		//fail("Write test case here");
+		assertEquals(0,Double.compare(7.5,CAD.valueInUSD(10)));
+		assertEquals(7.5,CAD.valueInUSD(10),2); //Alternative Assert
+		assertEquals(0,Double.compare(11.4,EUR.valueInUSD(10)));
+		assertEquals(11.4,EUR.valueInUSD(10),2); //Alternative Assert
+		assertEquals(0,Double.compare(12.6,GBP.valueInUSD(10)));
+		assertEquals(12.6,GBP.valueInUSD(10),2);
+		assertEquals(0,Double.compare(1.3,HKD.valueInUSD(10)));
+		assertEquals(1.3,HKD.valueInUSD(10),2);
 	}
 	
 	@Test
@@ -64,7 +81,12 @@ public class CurrencyTest {
 		// 2. Get the value in "this" currency
 		// 3. Assert that the value in "this" currency is correct
 		// You will end up with 2 assert() statements in this function.
-		fail("Write test case here");
+		//fail("Write test case here");
+		//assertEquals(0,Double.compare(12.6,CAD.valueInThisCurrency()));
+		assertEquals(15.2,CAD.valueInThisCurrency(10,EUR),2);
+		assertEquals(0,Double.compare(15.2,CAD.valueInThisCurrency(10,EUR)));
+		assertEquals(16.8,CAD.valueInThisCurrency(10,GBP),2);
+		assertEquals(1.73,CAD.valueInThisCurrency(10,HKD),2);
 	}
 
 }
