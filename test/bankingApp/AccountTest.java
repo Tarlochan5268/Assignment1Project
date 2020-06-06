c5fb4863f92d648ca767a010ab768afa27e16826
Tarlochan5268 <brotherstarlochan5268@gmail.com>
2020-06-06 07:56:42 -0400

    AccountTest CLASS DONE COMPLETE FIXED

--- test/bankingApp/AccountTest.java
+++ test/bankingApp/AccountTest.java
@@ -6,25 +6,25 @@ import org.junit.jupiter.api.Test;
 import static org.junit.Assert.*;
 
 public class AccountTest {
-	protected Currency CAD, HKD;
+	protected Currency HKD = new Currency("HKD", 0.13);
+	protected Currency CAD = new Currency("CAD", 0.75);
 	protected Bank TD;
 	protected Bank HSBC;
-	protected Bank RBC;
-	protected Account testAccount;
+	protected Bank RBC = new Bank("Royal Bank of Canada", CAD);
+	protected Account testAccount,testAccount2 = new Account("Test", CAD);
+
 	
 	@Before
 	public void setUp() throws Exception {
 		
 		// setup test currencies
-		CAD = new Currency("CAD", 0.75);
-		HKD = new Currency("CAD", 0.13);
-		RBC = new Bank("Royal Bank of Canada", CAD);
-		
+
 		// setup test accounts
 		RBC.openAccount("Peter");
 
 		testAccount = new Account("Albert", CAD);
 		testAccount.deposit(new Money(100, CAD));
+		//testAccount2.deposit(new Money(100, CAD));
 
 		// setup an initial deposit
 		RBC.deposit("Peter", new Money(500, CAD));
@@ -37,12 +37,28 @@ public class AccountTest {
 		//System.out.println(testAccount.toString()); //@Null Pointer Fail
 		//testAccount.withdraw(new Money(10,HKD));
 		//assertEquals(0,testAccount.getBalance());
+		testAccount2.deposit(new Money(100, CAD));
+		assertEquals(100,testAccount2.getBalance().getAmount(),0.001);
+		//System.out.println("Amount: "+testAccount2.getBalance().getAmount());
 
+		testAccount2.withdraw(new Money(20, CAD));
+		assertEquals(80,testAccount2.getBalance().getAmount(),0.001);
+		//check if we can withdraw money in different currency
+		testAccount2.withdraw(new Money(10, HKD));
+		assertEquals(78.27,testAccount2.getBalance().getAmount(),0.001); //@ WORKS PERFECTLY
 	}
 	
 	@Test
 	public void testGetBalance() {
+		testAccount2.deposit(new Money(100, CAD));
+		assertEquals(100,testAccount2.getBalance().getAmount(),0.001);
+		testAccount2.deposit(new Money(50, CAD));
+		assertEquals(150,testAccount2.getBalance().getAmount(),0.001);
+		//@Different currency deposit -> will it work ?
+		testAccount2.deposit(new Money(10,HKD)); //THIS works as well
+		assertEquals(151.73,testAccount2.getBalance().getAmount(),0.001);
+		//testAccount2.getBalance();
 		//fail("Write test case here");
-		testAccount.getBalance();
+		//testAccount.getBalance();
 	}
 }
