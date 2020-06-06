9dd8c5bb50ced2c3d238630228a9cfc551688953
Tarlochan5268 <brotherstarlochan5268@gmail.com>
2020-06-06 19:28:53 -0400

    Fixed Account Exist Exception Class

--- src/bankingApp/AccountExistsException.java
+++ src/bankingApp/AccountExistsException.java
@@ -1,5 +1,9 @@
 package bankingApp;
 
 public class AccountExistsException extends Exception {
-	static final long serialVersionUID = 1L; 
+	static final long serialVersionUID = 1L;
+	public AccountExistsException(String message)
+	{
+		super(message);
+	}
 }
